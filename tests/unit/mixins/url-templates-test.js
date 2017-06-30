8640b289e4cadbd169d47e2a805fe0cb1563d0b2
Amiel Martin <amiel@actionsprout.com>
2017-06-30 09:05:49 -0700

    Update tests to use const instead of var

--- tests/unit/mixins/url-templates-test.js
+++ tests/unit/mixins/url-templates-test.js
@@ -2,27 +2,31 @@ import Ember from 'ember';
 import UrlTemplatesMixin from 'ember-data-url-templates/mixins/url-templates';
 import { module, test } from 'qunit';
 
+const {
+  String: { camelize },
+} = Ember;
+
 module('UrlTemplatesMixin');
 
-var Adapter = Ember.Object.extend({
+const Adapter = Ember.Object.extend({
   pathForType(type) {
-    return Ember.String.camelize(type) + 's';
+    return camelize(type) + 's';
   }
 });
 
-var BasicAdapter = Adapter.extend(UrlTemplatesMixin, {
+const BasicAdapter = Adapter.extend(UrlTemplatesMixin, {
   urlTemplate: '/posts{/id}'
 });
 
-var NestedAdapter = Adapter.extend(UrlTemplatesMixin, {
+const NestedAdapter = Adapter.extend(UrlTemplatesMixin, {
   urlTemplate: '/posts/{postId}/comments{/id}'
 });
 
-var GenericAdapter = Adapter.extend(UrlTemplatesMixin, {
+const GenericAdapter = Adapter.extend(UrlTemplatesMixin, {
   urlTemplate: '{+host}{/namespace}/{pathForType}{/id}{?query*}'
 });
 
-var SegmentAdapter = Adapter.extend(UrlTemplatesMixin, {
+const SegmentAdapter = Adapter.extend(UrlTemplatesMixin, {
   urlTemplate: '/users/{userId}/posts{/category}',
   sessionId: 123,
 
@@ -35,124 +39,124 @@ var SegmentAdapter = Adapter.extend(UrlTemplatesMixin, {
   },
 });
 
-var OriginalAdapter = Adapter.extend({
+const OriginalAdapter = Adapter.extend({
   buildURL() {
     return 'posts-finder';
   }
 });
 
-var FallbackAdapter = OriginalAdapter.extend(UrlTemplatesMixin);
+const FallbackAdapter = OriginalAdapter.extend(UrlTemplatesMixin);
 
 test('it uses the template', function(assert) {
-  var subject = BasicAdapter.create();
-  var url = subject.buildURL();
+  const subject = BasicAdapter.create();
+  const url = subject.buildURL();
   assert.equal(url, '/posts');
 });
 
 test('it fills in the id', function(assert) {
-  var subject = BasicAdapter.create();
-  var url = subject.buildURL('post', 3);
+  const subject = BasicAdapter.create();
+  const url = subject.buildURL('post', 3);
   assert.equal(url, '/posts/3');
 });
 
 test('it escapes basic values', function(assert) {
-  var subject = BasicAdapter.create({ urlTemplate: '/{foo}' });
-  var url = subject.buildURL('post', null, { foo: 'bar baz' });
+  const subject = BasicAdapter.create({ urlTemplate: '/{foo}' });
+  const url = subject.buildURL('post', null, { foo: 'bar baz' });
   assert.equal(url, '/bar%20baz');
 });
 
 test('it escapes the id', function(assert) {
-  var subject = BasicAdapter.create();
-  var url = subject.buildURL('post', 'abc xyz');
+  const subject = BasicAdapter.create();
+  const url = subject.buildURL('post', 'abc xyz');
   assert.equal(url, '/posts/abc%20xyz');
 });
 
 test('it can use values from the snapshot', function(assert) {
-  var subject = NestedAdapter.create();
-  var url = subject.buildURL('comment', 5, { postId: 3 });
+  const subject = NestedAdapter.create();
+  const url = subject.buildURL('comment', 5, { postId: 3 });
   assert.equal(url, '/posts/3/comments/5');
 });
 
 test('it includes the namespace from the adapter', function(assert) {
-  var subject = GenericAdapter.create({ namespace: 'api' });
-  var url = subject.buildURL('post');
+  const subject = GenericAdapter.create({ namespace: 'api' });
+  const url = subject.buildURL('post');
   assert.equal(url, '/api/posts');
 });
 
 test('it includes the unescaped host from the adapter', function(assert) {
-  var subject = GenericAdapter.create({
+  const subject = GenericAdapter.create({
     host: 'http://example.com',
     namespace: 'api'
   });
 
-  var url = subject.buildURL('post');
+  const url = subject.buildURL('post');
   assert.equal(url, 'http://example.com/api/posts');
 });
 
 test('it can fill real query params', function(assert) {
-  var subject = BasicAdapter.create({ urlTemplate: '/posts{?date,category,tag}' });
-  var url = subject.buildURL('post', null, { date: '2015-10-10', tag: 'tagged' });
+  const subject = BasicAdapter.create({ urlTemplate: '/posts{?date,category,tag}' });
+  const url = subject.buildURL('post', null, { date: '2015-10-10', tag: 'tagged' });
   assert.equal(url, '/posts?date=2015-10-10&tag=tagged');
 });
 
 test('it can use real query params provided by the new query parameter', function(assert) {
-  var subject = BasicAdapter.create({ urlTemplate: '/posts{?date,category,tag}' });
-  var url = subject.buildURL('post', null, null, 'query', { date: '2015-10-10', tag: 'tagged' });
+  const subject = BasicAdapter.create({ urlTemplate: '/posts{?date,category,tag}' });
+  const url = subject.buildURL('post', null, null, 'query', { date: '2015-10-10', tag: 'tagged' });
   assert.equal(url, '/posts?date=2015-10-10&tag=tagged');
 });
 
 test('it can use a custom template for the request type', function(assert) {
-  var subject = BasicAdapter.create({ createRecordUrlTemplate: '/users/{userId}/create_post' });
-  var url = subject.buildURL('post', null, { userId: 1 }, 'createRecord');
+  const subject = BasicAdapter.create({ createRecordUrlTemplate: '/users/{userId}/create_post' });
+  const url = subject.buildURL('post', null, { userId: 1 }, 'createRecord');
   assert.equal(url, '/users/1/create_post');
 });
 
 test('it does not fail for missing values when there is no snapshot', function(assert) {
-  var subject = NestedAdapter.create();
-  var url = subject.buildURL('comment');
+  const subject = NestedAdapter.create();
+  const url = subject.buildURL('comment');
   assert.equal(url, '/posts//comments');
 });
 
 test('it uses urlSegments', function(assert) {
-  var subject = SegmentAdapter.create();
-  var url = subject.buildURL('post');
+  const subject = SegmentAdapter.create();
+  const url = subject.buildURL('post');
   assert.equal(url, '/users/123/posts');
 });
 
 test('it calls urlSegments with the query', function(assert) {
-  var subject = SegmentAdapter.create();
-  var url = subject.buildURL('post', null, null, 'find', { featured: true });
+  const subject = SegmentAdapter.create();
+  const url = subject.buildURL('post', null, null, 'find', { featured: true });
   assert.equal(url, '/users/123/posts/featured');
 });
 
 test('it falls back to original buildURL if no template is found for requestType', function(assert) {
-  var subject = FallbackAdapter.create();
-  var url = subject.buildURL();
+  const subject = FallbackAdapter.create();
+  const url = subject.buildURL();
   assert.equal(url, 'posts-finder');
 });
 
 test('it deletes undefined query parameter', function(assert) {
-  var subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
-  var url = subject.buildURL('post', null, null, 'query', { tag: undefined });
+  const subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
+  const url = subject.buildURL('post', null, null, 'query', { tag: undefined });
   assert.equal(url, '/posts');
 });
 
 test('it clears null query parameter', function(assert) {
-  var subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
-  var url = subject.buildURL('post', null, null, 'query', { tag: null });
+  const subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
+  const url = subject.buildURL('post', null, null, 'query', { tag: null });
   assert.equal(url, '/posts?tag=');
 });
 
 test('it does not mutate undefined query param', function(assert) {
-  var subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
-  var params = { tag: undefined };
+  const subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
+  const params = { tag: undefined };
   subject.buildURL('post', null, null, 'query', params);
   assert.ok('tag' in params);
 });
 
 test('it does not mutate null query param', function(assert) {
-  var subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
-  var params = { tag: null };
+  const subject = BasicAdapter.create({ urlTemplate: '/posts{?query*}' });
+  const params = { tag: null };
   subject.buildURL('post', null, null, 'query', params);
   assert.equal(params.tag, null);
 });
