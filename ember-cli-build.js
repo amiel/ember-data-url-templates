b0c2e623d10ddcb91c7030396554114a83c9e89c
Sam De Maeyer <sam@showmyhomework.co.uk>
2018-10-19 23:07:18 +0100

    Upgrade ember

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,11 +1,10 @@
-/* eslint-env node */
+'use strict';
+
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    'ember-cli-babel': {
-      includePolyfill: true
-    },
+  let app = new EmberAddon(defaults, {
+    // Add options here
   });
 
   /*
--- tests/dummy/app/routes/post.js
+++ tests/dummy/app/routes/post.js
@@ -1,21 +1,7 @@
-import Ember from 'ember';
-import RSVP from 'rsvp';
+import Route from '@ember/routing/route';
 
-const { get } = Ember;
-
-export default Ember.Route.extend({
-  model(params) {
-    return this.store.queryRecord('post', { slug: params.slug });
-  },
-
-  afterModel(model) {
-    // HACK: This prevents an issue introduced in Ember Data 2.14 (https://github.com/emberjs/data/issues/4942)
-    // This can safely be removed when emberjs/data#4942 has been resolved.
-    return RSVP.hash({
-      author: get(model, 'author'),
-      comments: get(model, 'comments'),
-      reactions: get(model, 'reactions'),
-      relatedPosts: get(model, 'relatedPosts'),
-    });
-  },
+export default Route.extend({
+  model({ slug }) {
+    return this.store.queryRecord('post', { slug });
+  }
 });
