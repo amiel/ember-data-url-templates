ea4a052442439fb429dc2ea994802d367b3c123d
Jones, Michael Dean (Contractor) <MDJONES@associates.nsf.gov>
2022-01-03 11:55:29 -0500

    independently import uri-templates

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  included(app) {
+    this._super.included.apply(this, arguments);
+    app.import('node_modules/uri-templates/uri-templates.js');
+    app.import('vendor/shims/uri-templates.js');
+  },
 };
---
+++ vendor/shims/uri-templates.js
@@ -0,0 +1,12 @@
+(function() {
+  function vendorModule() {
+    'use strict';
+
+    return {
+      'default': self['UriTemplate'],
+      __esModule: true,
+    };
+  }
+
+  define('uri-templates', [], vendorModule);
+})();
